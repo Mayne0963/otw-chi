570065db020dcff7cbc7166e7683c87aac7b47af
Mayne <00broskis@gmail.com>
2025-12-19 10:24:37 -0500

    Fix Clerk sign-in/up routes for path routing

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="min-h-[calc(100vh-80px)] flex items-center justify-center p-6">
-      <SignUp />
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <SignUp routing="path" path="/sign-up" />
     </div>
   );
 }
